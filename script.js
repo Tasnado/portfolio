bdb0275879e1249a0626ba00e9b4a3c81f325217
tasnia <tasnianabila1@gmail.com>
2021-10-20 23:30:24 -0400

    added functionality

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("it worked!");
+
+const init = () => {
+    const burgerMenu = document.querySelector("#burgerMenu");
+    const projects = document.querySelectorAll(".projects");
+    
+    burgerMenu.addEventListener('click', () => {
+        const navigation = document.querySelector("#navigation");
+        navigation.classList.toggle("show");
+    });
+
+    projects.forEach((project) => {
+        const projectImage = project.querySelector(".projectImage");
+        const textContent = project.querySelector("#projectTextContent");
+        projectImage.addEventListener('click', () => {
+            textContent.classList.toggle("showText");
+        });
+
+        textContent.addEventListener('click', function() {
+            textContent.classList.toggle("showText");
+        })
+    });
+}
+
+init();
